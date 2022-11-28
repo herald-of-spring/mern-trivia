1db810304e8a4f6fb6fcc6e0b1faba6e26c527e3
Amberlie Porter <whenamesattacks@gmail.com>
2022-11-27 20:44:44 -0800

    fixed issues with base and is now functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Main from './components/Main';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
+
 import './App.css'; 
+import Base from './components/base'
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
-
-function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+const App = props => {
+  return(
+    <section>
+      <Base />
+    </section>
+  )
 }
 
 export default App;
